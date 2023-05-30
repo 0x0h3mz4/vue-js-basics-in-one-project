db30a6aca54f1f5e9db27e8502beeac3ebaf7ed4
Hamza AitBamoh <hamzaaitbamoh@gmail.com>
2023-05-30 14:46:20 +0100

    added tags

--- part-5/components/todo-part.js
+++ part-5/components/todo-part.js
@@ -3,14 +3,35 @@ export default {
     components: { assignment },
     template: `
     <div class="row m-4" v-show="assignments.length">
-    <h2>{{title}}</h2>
-        <ul>
-            <assignment  v-for="item in assignments" :key="item.id" :item="item" ></assignment>
-        </ul>
+            
+                <h2>{{title}} <span>({{assignments.length}})</span></h2>
+                
+
+                <button @click="currenTag = tag" v-for="tag in tags"  class="m-1" :class="{'btn btn-primary' : tag==currenTag}"> {{tag}}</button>
+                <ul>
+                    <assignment  v-for="item in filteredassignments" :key="item.id" :item="item" ></assignment>
+                </ul>
+
     </div>`,
+    data() {
+        return {
+            currenTag: ''
+        }
+    },
     props: {
         assignments: Array,
         title: String,
+    },
+    computed: {
+        filteredassignments() {
+            if (this.currenTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag == this.currenTag);
+        },
+        tags() {
+            return ['all', ...new Set(this.assignments.map(a => a.tag))]
+        }
     }
 
 }
--- part-5/components/todo.js
+++ part-5/components/todo.js
@@ -14,9 +14,9 @@ export default {
     data() {
         return {
             list: [
-                { name: 'Go to the gym', complete: false, id: 1 },
-                { name: 'Finish the X project', complete: false, id: 2 },
-                { name: 'Read Thus spoke zarathustra', complete: false, id: 3 }
+                { name: 'Go to the gym', complete: false, id: 1, tag: "health" },
+                { name: 'Finish the X project', complete: false, id: 2, tag: "studies" },
+                { name: 'Read Thus spoke zarathustra', complete: false, id: 3, tag: "side hustle" }
             ]
         }
     }
